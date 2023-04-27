3e7879581e4dc6a109c3af5a9df300055168c087
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-04-27 17:41:40 -0500

    Add dynamic start end dates

--- src/components/TickerForm.js
+++ src/components/TickerForm.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 export default function TickerForm({searchParams, setStockData, setSearchParams}) {
 
+    
     async function callAPI () {
 
         const {ticker, multiplyer, timeUnit, startDate, endDate, apiKey} = searchParams
         try{
-          const res = await fetch(`https://api.polygon.io/v2/aggs/ticker/${ticker}/range/${multiplyer}/${timeUnit}/2023-01-09/2023-01-09?adjusted=true&sort=asc&limit=50000&apiKey=${apiKey}`)
+          const res = await fetch(`https://api.polygon.io/v2/aggs/ticker/${ticker}/range/${multiplyer}/${timeUnit}/${startDate}/${endDate}?adjusted=true&sort=asc&limit=50000&apiKey=${apiKey}`)
           const data = await res.json()
           setStockData(data.results)
           console.log(data)
@@ -18,10 +19,12 @@ export default function TickerForm({searchParams, setStockData, setSearchParams}
       const searchStock = (e) => {
         e.preventDefault()
 
+        console.log(searchParams)
         callAPI()
       }
 
       const modifySearchParams = (e) => {
+
 
         setSearchParams({
             ...searchParams,
