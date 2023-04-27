f9edfaf4c46e41203269731c681b9db6c4e576eb
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-04-27 18:14:14 -0500

    Create variables

--- src/components/BoxPlotComponent.js
+++ src/components/BoxPlotComponent.js
@@ -11,57 +11,75 @@ export default function BoxPlotComponent({stockData}) {
       const width = 500;
       const margin = { top: 20, right: 30, bottom: 30, left: 40 };
 
-      const x = d3
+      //Compute Variables
+      /** 
+      const X = d3
         .scaleBand()
         .domain(data.map((d) => d.t))
         .rangeRound([margin.left, width - margin.right])
         .padding(0.1);
         
-      const y1 = d3
+      const Yo = d3
         .scaleLinear()
-        .domain([0, d3.max(data, (d) => d.c)])
+        .domain([0, d3.max(data, (d) => d.o)])
         .rangeRound([height - margin.bottom, margin.top]);
+      */
 
-      const xAxis = (g) => g.attr("transform", `translate(0,${height - margin.bottom})`).call(
-          d3
-            .axisBottom(x)
-            .tickValues(
-              d3
-                .ticks(...d3.extent(x.domain()), width / 40)
-                .filter((v) => x(v) !== undefined)
-            )
-            .tickSizeOuter(0)
-        );
-      const y1Axis = (g) =>
-        g
-          .attr("transform", `translate(${margin.left},0)`)
-          .style("color", "steelblue")
-          .call(d3.axisLeft(y1).ticks(null, "s"))
-          .call((g) => g.select(".domain").remove())
-          .call((g) =>
-            g
-              .append("text")
-              .attr("x", -margin.left)
-              .attr("y", 10)
-              .attr("fill", "currentColor")
-              .attr("text-anchor", "start")
-              .text(data.y1)
-          );
+        const X = d3.map(data, d => d.t)
+        const Yo = d3.map(data, d => d.o)
+        const Yc = d3.map(data, d => d.c)
+        const Yh = d3.map(data, d => d.h)
+        const Yl = d3.map(data, d => d.l)
+        const L = d3.range(X.length)
 
-      svg.select(".x-axis").call(xAxis);
-      svg.select(".y-axis").call(y1Axis);
+        const weeks = (start, stop, stride) => d3.utcMonday.every(stride).range(start, +stop + 1);
+        const weekdays = (start, stop) => d3.utcDays(start, +stop + 1).filter(d => d.getUTCDay() !== 0 && d.getUTCDay() !== 6);
+        
 
-      svg
-        .select(".plot-area")
-        .attr("fill", "steelblue")
-        .selectAll(".bar")
-        .data(data)
-        .join("rect")
-        .attr("class", "bar")
-        .attr("x", (d) => x(d.t))
-        .attr("width", x.bandwidth())
-        .attr("y", (d) => y1(d.c))
-        .attr("height", (d) => y1(0) - y1(d.c));
+
+      
+
+      // const xAxis = (g) => g.attr("transform", `translate(0,${height - margin.bottom})`).call(
+      //     d3
+      //       .axisBottom(X)
+      //       .tickValues(
+      //         d3
+      //           .ticks(...d3.extent(X.domain()), width / 40)
+      //           .filter((v) => X(v) !== undefined)
+      //       )
+      //       .tickSizeOuter(0)
+      //   );
+      // const YoAxis = (g) =>
+      //   g
+      //     .attr("transform", `translate(${margin.left},0)`)
+      //     .style("color", "steelblue")
+      //     .call(d3.axisLeft(Yo).ticks(null, "s"))
+      //     .call((g) => g.select(".domain").remove())
+      //     .call((g) =>
+      //       g
+      //         .append("text")
+      //         .attr("x", -margin.left)
+      //         .attr("y", 10)
+      //         .attr("fill", "currentColor")
+      //         .attr("text-anchor", "start")
+      //         .text(data.Yo)
+      //     );
+
+
+      // svg.select(".x-axis").call(xAxis);
+      // svg.select(".y-axis").call(YoAxis);
+
+      // svg
+      //   .select(".plot-area")
+      //   .attr("fill", "steelblue")
+      //   .selectAll(".bar")
+      //   .data(data)
+      //   .join("rect")
+      //   .attr("class", "bar")
+      //   .attr("x", (d) => X(d.t))
+      //   .attr("width", X.bandwidth())
+      //   .attr("y", (d) => Yo(d.c))
+      //   .attr("height", (d) => Yo(0) - Yo(d.c));
 
     },
     [data.length]
