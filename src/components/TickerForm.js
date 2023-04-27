177bcf81fc541adaa92ffc514d7057c990ba85cd
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-04-27 13:27:33 -0500

    Create a select box for timespan

--- src/components/TickerForm.js
+++ src/components/TickerForm.js
@@ -6,7 +6,7 @@ export default function TickerForm({searchParams, setStockData, setSearchParams}
 
         const {ticker, multiplyer, timeUnit, startDate, endDate, apiKey} = searchParams
         try{
-          const res = await fetch(`https://api.polygon.io/v2/aggs/ticker/${ticker}/range/${multiplyer}/hour/2023-01-09/2023-01-09?adjusted=true&sort=asc&limit=50000&apiKey=${apiKey}`)
+          const res = await fetch(`https://api.polygon.io/v2/aggs/ticker/${ticker}/range/${multiplyer}/${timeUnit}/2023-01-09/2023-01-09?adjusted=true&sort=asc&limit=50000&apiKey=${apiKey}`)
           const data = await res.json()
           setStockData(data.results)
           console.log(data)
@@ -25,7 +25,7 @@ export default function TickerForm({searchParams, setStockData, setSearchParams}
 
         setSearchParams({
             ...searchParams,
-            [e.target.name]: e.target.value.toUpperCase() //Maybe find a different way to do this
+            [e.target.name]: e.target.value
         })
 
         console.log(searchParams)
@@ -34,7 +34,15 @@ export default function TickerForm({searchParams, setStockData, setSearchParams}
 
   return (
     <form onSubmit={searchStock}>
-        <input type="text" name="ticker" autoCapitalize='words' onChange={modifySearchParams}/>
+        <input type="text" name="ticker" onChange={modifySearchParams}/>
+        <label htmlFor="timeUnit">Time span:</label>
+        <select name="timeUnit" onChange={modifySearchParams}>
+            <option value="hour">Hour</option>
+            <option value="week">Week</option>
+            <option value="month">Month</option>
+            <option value="quarter">Quarter</option>
+            <option value="year">Year</option>
+        </select>
         <button type="submit">Search</button>
     </form>
   )
