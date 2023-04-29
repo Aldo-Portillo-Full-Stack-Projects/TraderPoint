f9071a477238c1780f3332bea5a5cba86caeab09
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-04-28 22:16:44 -0500

    Edit dates

--- src/components/CandleStickApex.js
+++ src/components/CandleStickApex.js
@@ -1,30 +1,57 @@
+import dayjs from "dayjs";
 import React from "react";
 import Chart from "react-apexcharts";
 
 function CandleStickApex ({formattedData}) {
     const options = {
-        xaxis:{
-            labels: {
-                format: 'dd/MM',
-              }
-        },
         chart: {
-            type: "candlestick"
+          height: 350,
+          type: 'candlestick',
         },
-        candlestick: {
-            colors: {
-                upward: '#3C90EB',
-                downward: '#DF7D46'
-            },
-            wick: {
-                useFillColor: true,
+        title: {
+          text: 'CandleStick Chart - Category X-axis',
+          align: 'left'
+        },
+        annotations: {
+          xaxis: [
+            {
+              x: 'Oct 06 14:00',
+              borderColor: '#00E396',
+              label: {
+                borderColor: '#00E396',
+                style: {
+                  fontSize: '12px',
+                  color: '#fff',
+                  background: '#00E396'
+                },
+                orientation: 'horizontal',
+                offsetY: 7,
+                text: 'Annotation Test'
+              }
             }
+          ]
         },
-    }
-    const [series, setSeries] = React.useState([{data: formattedData}])
+        tooltip: {
+          enabled: true,
+        },
+        xaxis: {
+          type: 'category',
+          labels: {
+            formatter: function(val) {
+              return dayjs(val).format('MMM DD HH:mm')
+            }
+          }
+        },
+        yaxis: {
+          tooltip: {
+            enabled: true
+          }
+        }
+      }
+    const [series, setSeries] = React.useState([{name: 'candle', data: formattedData}])
 
     React.useEffect(() => {
-        setSeries([{data: formattedData}])
+        setSeries([{name: 'candle', data: formattedData}])
     }, [formattedData])
     return (
         <div className="app">
@@ -35,7 +62,6 @@ function CandleStickApex ({formattedData}) {
                 series={series}
                 type="candlestick"
                 width="500"
-                xaxis={options.xaxis}
               />
             </div>
           </div>
